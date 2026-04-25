bbb26c27ae7f6712c9fb2a3101b1f32091d4c7d2
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-04-25 13:45:43 -0300

    CHORE: Re-inserting status on payments and erasing redundant column.

--- db/migrate/20260425071247_create_payments.rb
+++ db/migrate/20260425071247_create_payments.rb
@@ -2,7 +2,7 @@ class CreatePayments < ActiveRecord::Migration[8.1]
   def change
     create_table :payments do |t|
       t.decimal :amount, precision: 10, scale: 2, null: false
-      t.string :payable_type
+      t.integer :status, default: 0, null: false
 
       t.references :payable, polymorphic: true, null: false
 
