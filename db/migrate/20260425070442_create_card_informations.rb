54becf39cbd61cd6086655cdd811c0eb9e4e730e
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-04-25 04:08:29 -0300

    CHORE: Adding Card Information table.

---
+++ db/migrate/20260425070442_create_card_informations.rb
@@ -0,0 +1,14 @@
+class CreateCardInformation < ActiveRecord::Migration[8.1]
+  def change
+    create_table :card_information do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :card_number
+      t.string :card_owner
+      t.integer :card_verification_number
+      t.string :card_owner_document
+      t.boolean :active
+
+      t.timestamps
+    end
+  end
+end
