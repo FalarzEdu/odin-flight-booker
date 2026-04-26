46629004171cb8f3d479908e84c9439d1cce8d82
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-04-26 01:41:22 -0300

    CHORE: Creating seeds for cities and airports.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+cities = [
+  { state: "PR", country: "BRA", name: "Curitiba", abbreviation: "CWB" },
+  { state: "PR", country: "BRA", name: "Maringá", abbreviation: "MGF" },
+  { state: "SP", country: "BRA", name: "São Paulo", abbreviation: "SAO" },
+  { state: "MG", country: "BRA", name: "Belo Horizonte", abbreviation: "BHZ" },
+  { state: "DF", country: "BRA", name: "Brasília", abbreviation: "BSB" }
+]
+
+cities.each do |city_details|
+  City.find_or_create_by!(
+    state: city_details[:state],
+    country: city_details[:country],
+    name: city_details[:name],
+    abbreviation: city_details[:abbreviation]
+  )
+end
+
+airports = [
+  { code: "GRU", city_abbreviation: "SAO" },
+  { code: "CGH", city_abbreviation: "SAO" },
+  { code: "VCP", city_abbreviation: "SAO" },
+  { code: "CWB", city_abbreviation: "CWB" }
+]
+
+airports.each do |airport_details|
+  city = City.find_by(abbreviation: airport_details[:city_abbreviation])
+
+  Airport.find_or_create_by!(
+    code: airport_details[:code],
+    city_id: city[:id]
+  )
+end
