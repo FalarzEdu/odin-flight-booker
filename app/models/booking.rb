15f374a0ed99268c5379bd0b68b691bd5a5f2404
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-01 11:40:45 -0300

    FEAT: Adding nested attributes and validation for passengers relation on Booking model.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,10 +3,13 @@ class Booking < ApplicationRecord
   has_many :payments
   has_many :passengers
 
+  accepts_nested_attributes_for :passengers
+
   enum :status, { pending_payment: 0, confirmed: 1 }, default: :pending_payment
 
   validates :status, presence: true
   validates :flight, presence: true
   validates :total_paid, presence: true,
             numericality: { greater_than_or_equal_to: 0 }
+  validates_associated :passengers
 end
