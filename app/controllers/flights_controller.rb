0abd2a0dc1d6241cf8b43b93fb6f0575ada54498
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-01 11:05:21 -0300

    FEAT: Creating flights controller, it's index page and some scopes on the model.

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,25 @@
+class FlightsController < ApplicationController
+  def index
+    @flights = Flight.all
+
+    if filtering_params.present?
+      @flights = @flights.with_departure_airport(params[:departure_airport_id]) if params[:departure_airport_id].present?
+      @flights = @flights.with_arrival_airport(params[:arrival_airport_id]) if params[:arrival_airport_id].present?
+
+      if params[:departure_date].present?
+        date = Date.parse(params[:departure_date])
+        @flights = @flights.with_exact_departure_date(date)
+      end
+    end
+  end
+
+  private
+
+  def filtering_params
+    params.permit(
+      :departure_airport_id,
+      :arrival_airport_id,
+      :departure_date
+    ).select { |key, value| value.present? }
+  end
+end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,20 @@ class Flight < ApplicationRecord
   belongs_to :route
 
   has_many :bookings
+
+  scope :with_departure_airport, ->(id) {
+    joins(:route).where(routes: { departure_airport_id: id })
+  }
+
+  scope :with_arrival_airport, ->(id) {
+    joins(:route).where(routes: { arrival_airport_id: id })
+  }
+
+  scope :with_exact_departure_date, ->(date) {
+    where(departure_datetime: date)
+  }
+
+  def self.available_dates
+    all.map { |flight| flight.departure_datetime.to_date }.uniq.sort
+  end
 end
