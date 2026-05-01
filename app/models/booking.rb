d3163a926316bb15a2534964f4fc9f31bb436b51
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-01 11:35:56 -0300

    FEAT: Adding validations to Booking model.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,12 @@
 class Booking < ApplicationRecord
+  belongs_to :flight
+  has_many :payments
+  has_many :passengers
+
   enum :status, { pending_payment: 0, confirmed: 1 }, default: :pending_payment
+
+  validates :status, presence: true
+  validates :flight, presence: true
+  validates :total_paid, presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
 end
