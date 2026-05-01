0f5736c42cb4eee1ad307dba43c6db90e49cc1b2
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-01 12:37:46 -0300

    FEAT: Adding validations to passenger model.

---
+++ app/models/passenger.rb
@@ -0,0 +1,12 @@
+class Passenger < ApplicationRecord
+  belongs_to :booking
+
+  validates :name, presence: true
+  validates :date_of_birth, presence: true,
+                            comparison: {
+                              greater_than: 120.years.ago,
+                              less_than_or_equal_to: -> { Date.current },
+                              message: "must be a valid birth date"
+                            }
+  validates :booking, presence: true
+end
