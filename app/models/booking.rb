f1015cdbcb14c6cfc99d2ccf78b95e9f73b5befe
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-01 13:40:18 -0300

    FEAT: Adding link to booking and guard clause to prevent booking an old flight.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,7 +9,16 @@ class Booking < ApplicationRecord
 
   validates :status, presence: true
   validates :flight, presence: true
+  validate :flight_must_be_in_the_future, on: :create
   validates :total_paid, presence: true,
             numericality: { greater_than_or_equal_to: 0 }
   validates_associated :passengers
+
+  private
+
+  def flight_must_be_in_the_future
+    if flight.present? && flight.departure_datetime < Date.current
+      errors.add(:base, "Cannot book a flight that has already departed.")
+    end
+  end
 end
