4a11c36f2395a0f9d880e2ea21e4cbb08b8f7b7b
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-01 22:22:27 -0300

    CHORE: Deactivating Devise's rememberable module.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable, :recoverable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable, :rememberable, :recoverable and :omniauthable
+  devise :database_authenticatable, :registerable, :validatable
 end
