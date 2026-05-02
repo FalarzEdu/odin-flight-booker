9a7aeb3e8bf8cd7483a1c20850b2d24681ddacb7
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-02 15:56:43 -0300

    REFACTOR: Changing model columns names for validation.

--- app/models/card_information.rb
+++ app/models/card_information.rb
@@ -2,8 +2,8 @@ class CardInformation < ApplicationRecord
   belongs_to :user
   has_many :card_payments
 
-  validates :card_number, :card_owner, :card_owner_document, :active, presence: true
-  validates :card_verification_number, numericality: { only_integer: true },
+  validates :number, :owner_full_name, :owner_document, :active, presence: true
+  validates :verification_number, numericality: { only_integer: true },
                                        length: { minimum: 3, maximum: 4 }
   validates :user, presence: true
 end
