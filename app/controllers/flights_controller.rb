1b69c86300946c4d285bcc08740d5709511c4a59
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-01 22:57:04 -0300

    REFACTOR: Adopting early return for better readability.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,16 +2,16 @@ class FlightsController < ApplicationController
   def index
     @flights = nil
 
-    if filtering_params.present?
-      @flights = Flight.upcoming
+    return unless filtering_params.present?
 
-      @flights = @flights.with_departure_airport(params[:departure_airport_id]) if params[:departure_airport_id].present?
-      @flights = @flights.with_arrival_airport(params[:arrival_airport_id]) if params[:arrival_airport_id].present?
+    @flights = Flight.upcoming.includes(route: [ :departure_airport, :arrival_airport ])
 
-      if params[:departure_date].present?
-        date = Date.parse(params[:departure_date])
-        @flights = @flights.with_exact_departure_date(date)
-      end
+    @flights = @flights.with_departure_airport(params[:departure_airport_id]) if params[:departure_airport_id].present?
+    @flights = @flights.with_arrival_airport(params[:arrival_airport_id]) if params[:arrival_airport_id].present?
+
+    if params[:departure_date].present?
+      date = Date.parse(params[:departure_date])
+      @flights = @flights.with_exact_departure_date(date)
     end
   end
 
