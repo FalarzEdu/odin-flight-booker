50931da1fb10b951027920c385d7c50b09ecc791
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-02 14:50:51 -0300

    FEAT: Adding user validation and error alerts on edit form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :rememberable, :recoverable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
+
+  validates :date_of_birth, comparison: {
+                                          greater_than_or_equal_to: 120.year.ago,
+                                          lesser_than: -> { Time.current },
+                                          message: "must be a valid birth date"
+                                        }
+  validates :name, length: { minimum: 2, maximum: 100 }
 end
