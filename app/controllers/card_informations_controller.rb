5c0fef71ad97c3a6cfc93c4335ccbdf74eff5d3a
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-02 18:29:54 -0300

    FEAT: Adding delete button to active cards.

--- app/controllers/card_informations_controller.rb
+++ app/controllers/card_informations_controller.rb
@@ -18,13 +18,24 @@ class CardInformationsController < ApplicationController
     end
   end
 
+  def destroy
+    @card_information = current_user.card_informations.find(params[:id])
+
+    if @card_information.update(active: false)
+      redirect_to profile_path, notice: "Card successfully deleted."
+    else
+      redirect_to profile_path, alert: "We could not remove that card right now."
+    end
+  end
+
   private
 
   def card_information_params
     params.expect(card_information: [
       :owner_full_name,
       :number,
-      :owner_document
+      :owner_document,
+      :_destroy
     ])
   end
 end
