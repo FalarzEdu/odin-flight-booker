2161a0d0e68002c4e76f0794860b1b6cc142109d
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-02 15:46:16 -0300

    CHORE: Fixing Rubocop offenses.

--- app/models/card_information.rb
+++ app/models/card_information.rb
@@ -3,7 +3,7 @@ class CardInformation < ApplicationRecord
   has_many :card_payments
 
   validates :card_number, :card_owner, :card_owner_document, :active, presence: true
-  validates :card_verification_number, numericality: { only_integer: true},
+  validates :card_verification_number, numericality: { only_integer: true },
                                        length: { minimum: 3, maximum: 4 }
   validates :user, presence: true
 end
