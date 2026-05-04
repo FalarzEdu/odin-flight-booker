62980d67f7cbc87621280e5f83b2bd42bf442b7f
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-03 23:12:21 -0300

    FEAT: Expanding sign up view to request name and birth date.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,12 @@ class ApplicationController < ActionController::Base
   stale_when_importmap_changes
 
   before_action :authenticate_user!
+  before_action :configure_devise_permitted_params, if: :devise_controller?
+
+  protected
+
+  def configure_devise_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :name, :date_of_birth ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :date_of_birth ])
+  end
 end
