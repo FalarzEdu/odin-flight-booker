1f90fd0eeb712caad58d5c838d084f6eb53b8a57
Eduardo Falarz <eduardofalarz23@gmail.com>
2026-05-05 21:39:59 -0300

    FIX: Using a lambda to calculate expiration time upon every Pix Payment object creation.

--- app/models/pix_payment.rb
+++ app/models/pix_payment.rb
@@ -3,10 +3,10 @@ class PixPayment < ApplicationRecord
 
   validates :issuing_bank, :transaction_id, :pix_key, :payload, presence: true
   validates :expires_at, presence: true,
-            numericality: true
-            # comparison: {
-            #   greater_than: 0,
-            #   less_than_or_equal_to: 30.minutes,
-            #   message: "a pix expiration can not be negative nor longer than 30 minutes"
-            # }
+            numericality: true,
+            comparison: {
+              greater_than: 0,
+              less_than_or_equal_to: -> { 30.minutes.from_now },
+              message: "a pix expiration can not be negative nor longer than 30 minutes"
+            }
 end
